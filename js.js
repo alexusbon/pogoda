afb2d3465a8b807fffba8165809aea053daf32df
artem-valuevich <artem.valuevich@mail.ru>
2021-02-19 17:41:10 +0300

    https

--- js.js
+++ js.js
@@ -4,7 +4,7 @@ import {  getFilteredData, render } from "./functions.js";
 async function getWeather(city = "Minsk") {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=c026b7e260ea6a4cea507080c2ca5e3f&units=metric`);
     const data = await response.json();
-    const timeZoneResponse = await fetch(`http://api.timezonedb.com/v2.1/get-time-zone?key=95WHPNRO6JMC&format=json&by=position&lat=${data.city.coord.lat}&lng=${data.city.coord.lon}`)
+    const timeZoneResponse = await fetch(`https://api.timezonedb.com/v2.1/get-time-zone?key=95WHPNRO6JMC&format=json&by=position&lat=${data.city.coord.lat}&lng=${data.city.coord.lon}`)
     const timeZoneData = await timeZoneResponse.json();
     const filteredData = getFilteredData(data, timeZoneData)
 
